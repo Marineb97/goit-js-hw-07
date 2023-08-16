10389309aec04cd99a77de09d4964dccec09e98a
Maryna Bludenko <marineblu@icloud.com>
2023-08-16 20:58:45 +0100

    updated

---
+++ js/01-gallery.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainer = document.querySelector('.gallery');
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+galleryContainer.addEventListener('click', onImgClick);
+
+// rendered items
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
