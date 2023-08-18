2e8a8af700a48f3342eeea789a30ff0083052670
Maryna Bludenko <marineblu@icloud.com>
2023-08-18 21:50:10 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,6 +6,7 @@ galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 galleryContainer.addEventListener('click', onImgClick);
 
 // rendered items
+
 function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
@@ -21,4 +22,36 @@ function createGalleryItemsMarkup(items) {
 </li>`;
     })
     .join('');
-}
+};
+
+function onImgClick(e) {
+  e.preventDefault();
+   if (e.target.nodeName !== "IMG") return;
+
+  const isItemImage = e.target.classList.contains('gallery__image');
+   if (!isItemImage) return;
+
+   const currentImgUrl = e.target.dataset.source;
+
+   const instance = basicLightbox.create(
+    `
+ 		<img src="${currentImgUrl}" width="1280" height="auto"/>
+        `,
+    {
+      onShow: (instance) => {
+        window.addEventListener('keydown', onEscKeyPress);
+     },
+    onClose: (instance) => {
+        window.removeEventListener('keydown', onEscKeyPress);
+      },
+    }  );
+ instance.show();
+
+ function onEscKeyPress(e) {
+     const ESC_KEY_CODE = 'Escape';
+     const isEscKey = e.code === ESC_KEY_CODE;
+     if (!isEscKey) return;
+     instance.close();
+   }
+ }
+
